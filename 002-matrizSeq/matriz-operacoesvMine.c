4be907b06f1103f0172f2523c8db4d9746ca4edb
Forvisk <adrianojr11@gmail.com>
2020-05-04 14:24:40 -0300

    "teste multiplicação"

---
+++ 002-matrizSeq/matriz-operacoesvMine.c
@@ -0,0 +1,76 @@
+#include "matriz-operacoesv3.h"
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MATRIZ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mymatriz *msomar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
+	mymatriz *mat_c = NULL;
+
+	if ((mat_a->lin != mat_b-> lin) || (mat_a->col != mat_b->col)){
+		printf ("Erro: Matrizes incompatíveis!\n");
+		exit(1);
+	}
+
+	mat_c = (mymatriz *) malloc (sizeof(mymatriz));
+	mat_c->lin = mat_a->lin;
+	mat_c->col = mat_a->col;
+
+	if (malocar(mat_c)) {	printf ("ERROR: Out of memory\n"); }
+
+	if (tipo == 1) {
+		for (int i=0; i < mat_c->lin; i++)
+		  for (int j=0; j < mat_c->col; j++)
+					mat_c->matriz[i][j] = mat_a->matriz[i][j]+mat_b->matriz[i][j];
+	} else {
+		for (int j=0; j < mat_c->col; j++)
+			for (int i=0; i < mat_c->lin; i++)
+					mat_c->matriz[i][j] = mat_a->matriz[i][j]+mat_b->matriz[i][j];
+	}
+  return mat_c;
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
+	mymatriz *mat_c = NULL;
+	int nMe = 0;
+
+	if ((mat_a->lin != mat_b->col) || (mat_a->col != mat_b->lin)){
+		printf ("Erro: Matrizes incompatíveis!\n");
+		exit(1);
+	}
+
+	if (tipo == 1){
+		mat_c = (mymatriz *) malloc (sizeof(mymatriz));
+		mat_c->lin = mat_a->lin;
+		mat_c->col = mat_b->col;
+		nMe = mat_b->lin;
+	} else {
+		mat_c = (mymatriz *) malloc (sizeof(mymatriz));
+		mat_c->lin = mat_b->lin;
+		mat_c->col = mat_a->col;
+		nMe = mat_b->col;
+	}
+
+	if (malocar(mat_c)) {	printf ("ERROR: Out of memory\n"); }
+
+	//Primeiro testemos a forma IDIOTA
+	if (tipo == 1) {
+		for( int i=0; i< mat_c->lin; i++)
+			for (int j = 0; j < mat_c->col; j++) {
+				mat_c->matriz[i][j] = 0
+				for (int k = 0; k < nMe; k++) {
+					mat_c->matriz[i][j] += mat_a->matriz[i][k] * mat_b->matriz[k][j];
+				}
+			}
+
+	}else{
+		for( int i=0; i< mat_c->lin; i++)
+			for (int j = 0; j < mat_c->col; j++) {
+				mat_c->matriz[i][j] = 0
+				for (int k = 0; k < nMe; k++) {
+					mat_c->matriz[i][j] += mat_a->matriz[k][j] * mat_b->matriz[i][k];
+				}
+			}
+	}
+
+	return mat_c;
+}
