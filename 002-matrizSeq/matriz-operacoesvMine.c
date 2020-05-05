3a42685113f3d1446d0c1f973442e7a0bcd17994
Forvisk <adrianojr11@gmail.com>
2020-05-05 12:19:08 -0300

    auto

--- 002-matrizSeq/matriz-operacoesvMine.c
+++ 002-matrizSeq/matriz-operacoesvMine.c
@@ -33,18 +33,18 @@ mymatriz *msomar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
 	mymatriz *mat_c = NULL;
-	int nMe = 0;
-	//tipo = tipo % 2;
+	tipo = tipo % 2;
+
 	if ((tipo == 0) && (mat_a->col == mat_b->lin)){
 		mat_c = (mymatriz *) malloc (sizeof(mymatriz));
 		mat_c->lin = mat_a->lin;
 		mat_c->col = mat_b->col;
-		nMe = mat_b->lin;
+
 	} else if ((tipo == 1) && (mat_a->lin == mat_b->col)){
 		mat_c = (mymatriz *) malloc (sizeof(mymatriz));
 		mat_c->lin = mat_b->lin;
 		mat_c->col = mat_a->col;
-		nMe = mat_a->lin;
+
 	}else{
 		printf("Matriz a: %d %d \n", mat_a->lin, mat_a->col);
 		printf("Matriz b: %d %d \n", mat_b->lin, mat_b->col);
@@ -55,11 +55,11 @@ mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
 	if (malocar(mat_c)) {	printf ("ERROR: Out of memory\n"); }
 	printf("here3\n");
 	//Primeiro testemos a forma IDIOTA
-	if (tipo == 1) {
+	if (tipo == 0) {
 		for( int i=0; i< mat_c->lin; i++)
 			for (int j = 0; j < mat_c->col; j++) {
 				mat_c->matriz[i][j] = 0;
-				for (int k = 0; k < nMe; k++) {
+				for (int k = 0; k < mat_a->col; k++) {
 					mat_c->matriz[i][j] += mat_a->matriz[i][k] * mat_b->matriz[k][j];
 				}
 			}
@@ -68,7 +68,7 @@ mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
 		for( int i=0; i< mat_c->lin; i++)
 			for (int j = 0; j < mat_c->col; j++) {
 				mat_c->matriz[i][j] = 0;
-				for (int k = 0; k < nMe; k++) {
+				for (int k = 0; k < mat_b->col; k++) {
 					mat_c->matriz[i][j] += mat_a->matriz[k][j] * mat_b->matriz[i][k];
 				}
 			}
