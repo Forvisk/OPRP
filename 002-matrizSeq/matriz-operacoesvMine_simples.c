2ecc628d25587209d2ea884ab2dfb4dd482c7d60
forvisk <war1164robe>
2020-06-25 09:42:57 -0300

    yep

---
+++ 002-matrizSeq/matriz-operacoesvMine_simples.c
@@ -0,0 +1,80 @@
+#include "matriz-operacoesv3.h"
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MATRIZ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mymatriz *msomar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
+	mymatriz *mat_c = NULL;
+	//printf("here soma 1\n");
+	if ((mat_a->lin != mat_b-> lin) || (mat_a->col != mat_b->col)){
+		printf("Matriz a: %d %d \n", mat_a->lin, mat_a->col);
+		printf("Matriz b: %d %d \n", mat_b->lin, mat_b->col);
+		printf ("Erro: Matrizes incompatíveis!\n");
+		exit(1);
+	}
+
+	mat_c = (mymatriz *) malloc (sizeof(mymatriz));
+	mat_c->lin = mat_a->lin;
+	mat_c->col = mat_a->col;
+
+	if (malocar(mat_c)) {	printf ("ERROR: Out of memory\n"); }
+
+	if (tipo == 1) {
+		for (int i=0; i < mat_c->lin; i++)
+		  for (int j=0; j < mat_c->col; j++)
+					mat_c->matriz[i][j] = mat_a->matriz[i][j]+mat_b->matriz[i][j];
+	} else {
+		for (int j=0; j < mat_c->col; j++)
+			for (int i=0; i < mat_c->lin; i++)
+					mat_c->matriz[i][j] = mat_a->matriz[i][j]+mat_b->matriz[i][j];
+	}
+  return mat_c;
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo) {
+	mymatriz *mat_c = NULL;
+	mymatriz *mat_aux = NULL;
+	tipo = tipo % 2;
+
+	if ((tipo == 0) && (mat_a->col == mat_b->lin)){
+		mat_c = (mymatriz *) malloc (sizeof(mymatriz));
+		mat_c->lin = mat_a->lin;
+		mat_c->col = mat_b->col;
+
+	} else if ((tipo == 1) && (mat_a->lin == mat_b->col)){
+		mat_c = (mymatriz *) malloc (sizeof(mymatriz));
+		mat_c->lin = mat_b->lin;
+		mat_c->col = mat_a->col;
+
+	}else{
+		printf("Matriz a: %d %d \n", mat_a->lin, mat_a->col);
+		printf("Matriz b: %d %d \n", mat_b->lin, mat_b->col);
+		printf ("Erro: Matrizes incompatíveis!\n");
+		exit(1);
+	}
+
+	if (malocar(mat_c)) {	printf ("ERROR: Out of memory\n"); }
+	
+	
+	//Primeiro testemos a forma IDIOTA
+	if (tipo == 0) {
+		for( int i=0; i< mat_c->lin; i++)
+			for (int j = 0; j < mat_c->col; j++) {
+				mat_c->matriz[i][j] = 0;
+				for (int k = 0; k < mat_a->col; k++) {
+					mat_c->matriz[i][j] += mat_a->matriz[i][k] * mat_b->matriz[k][j];
+				}
+			}
+	}else{
+		for( int i=0; i< mat_c->lin; i++)
+			for (int j = 0; j < mat_c->col; j++) {
+				mat_c->matriz[i][j] = 0;
+				for (int k = 0; k < mat_b->col; k++) {
+					mat_c->matriz[i][j] += mat_a->matriz[k][j] * mat_b->matriz[i][k];
+				}
+			}
+	}
+
+	return mat_c;
+}
+
